dc9ead9853057f9a7aa68337a7ce63f1b070bd46
Aleksey Ponomarev <exeyp@yandex.ru>
2024-07-31 17:50:56 +0800

    First commit

---
+++ graphql_client.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+class GraphQLClient:
+    def __init__(self, config_path):
+        with open(config_path, 'r', encoding='utf-8') as file:
+            self.config = json.load(file)
+        self.session = requests.Session()
+    
+    def login(self):
+        login_payload = {
+            'user': self.config['username'],
+            'password': self.config['password'],
+            'items': {'app': self.config['app']}
+        }
+        response = self.session.post(self.config['login_url'], json=login_payload, verify=False)
+        if response.status_code != 200:
+            response.raise_for_status()
+    
+    def logout(self):
+        response = self.session.post(self.config['logout_url'], verify=False)
+        if response.status_code != 200:
+            response.raise_for_status()
+
+    def execute_query(self, query_path, variables=None):
+        with open(query_path, 'r', encoding='utf-8') as file:
+            query = file.read()
+        response = self.session.post(self.config['graphql_endpoint'], json={'query': query, 'variables': variables}, verify=False)
+        if response.status_code != 200:
+            response.raise_for_status()
+        result = response.json()
+        if 'errors' in result:
+            raise Exception(f"GraphQL errors: {result['errors']}")
+        return result
+
+    def __enter__(self):
+        self.login()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.logout()
---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import json
+from graphql_client import GraphQLClient
+
+
+def main():
+    config_path = os.path.join('config', 'config.json')
+    with open(config_path, 'r', encoding='utf-8') as file:
+        config = json.load(file)
+    start_date = config['start_date']
+    end_date = config['end_date']
+
+    query_path = os.path.join('queries', 'query.graphql')
+    variables = {
+    "start_date": start_date,
+    "end_date": end_date
+    }
+
+    with GraphQLClient(config_path) as graphql_client:
+        response = graphql_client.execute_query(query_path, variables)
+        print(response)
+
+if __name__ == '__main__':
+    main()
