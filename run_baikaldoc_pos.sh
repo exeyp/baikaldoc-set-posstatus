45e862f320ef362f287e4e5bd57a7906ba66192f
Aleksey Ponomarev <exeyp@yandex.ru>
2024-08-09 13:07:34 +0800

    Add bash script

---
+++ run_baikaldoc_pos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Устанавливаем PYTHONPATH, , чтобы Python мог найти модуль 'app'
+export PYTHONPATH="$(dirname "$0")"
+
+# Путь к проекту
+PROJECT_DIR="/opt/baikaldoc-set-posstatus"
+
+# Активируем виртуальное окружение
+source "$PROJECT_DIR/.venv/bin/activate"
+
+# Запускаем приложение и направляем логи в syslog
+python "$PROJECT_DIR/app/main.py" 2>&1 | logger -t baikaldoc-pos
